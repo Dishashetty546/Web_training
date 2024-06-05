ecf0cffd2e338ed3f3cd7e341e0f35b9bf2221a3
dishashetty546 <dishashetty546@gmail.com>
2024-06-05 15:16:06 +0530

    this id changes code

---
+++ p3/p3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+printf(" hello im disha shetty");
+printf("im from udupi");
+printf("hellllllo");
+printf("hiiii");
+    return 0;
+}
